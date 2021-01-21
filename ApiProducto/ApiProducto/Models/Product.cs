760078cc5fb0be857d02b2da650cc14d78cd1c6b
Natalia Anez Safar <nataliaanezsafar@gmail.com>
2021-01-20 21:38:55 -0400

    Terminado.

---
+++ ApiProducto/ApiProducto/Models/Product.cs
@@ -0,0 +1,38 @@
+﻿namespace ApiProducto.Models
+{
+    using System.ComponentModel.DataAnnotations;
+    public enum MarketType
+    { 
+        Niños=0,
+        Jovenes=1,
+        Hombres=2,
+        Mujeres=3, 
+        Mayores=4
+    }
+
+    public enum CategoryType
+    { 
+        Ropa=0,
+        Electronicos=1,
+        Cosmeticos=2,
+        Muebleria=3,
+        Juegos=4,
+        Deportes=5,
+        Bebidas=6,
+        Alimentos=7
+    }
+
+    public class Product
+    {
+        [Key]
+        public int PoductId { get; set; }
+        [Required]
+        [StringLength(80,ErrorMessage ="El nombre debe contener entre 5 - 80 caracteres",MinimumLength =5)]
+        public string Name { get; set; }
+        [Range(0,10000,ErrorMessage ="El precio oscila entre 0 - 10000")]
+        public double Price{ get; set; }
+        public MarketType Market{ get; set; }
+        public CategoryType Category { get; set; }
+
+    }
+}
---
+++ WebProducto/Controllers/ProductsController.cs
@@ -0,0 +1,124 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Data.Entity.Infrastructure;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using System.Web.Http.Description;
+using ApiProducto.Models;
+using WebProducto.Models;
+
+namespace WebProducto.Controllers
+{
+    public class ProductsController : ApiController
+    {
+        private DataContext db = new DataContext();
+
+        // GET: api/Products
+        [Authorize]
+        public IQueryable<Product> GetProducts()
+        {
+            return db.Products;
+        }
+
+        [Authorize]
+        // GET: api/Products/5
+        [ResponseType(typeof(Product))]
+        public IHttpActionResult GetProduct(int id)
+        {
+            Product product = db.Products.Find(id);
+            if (product == null)
+            {
+                return NotFound();
+            }
+
+            return Ok(product);
+        }
+
+        // PUT: api/Products/5
+        [Authorize]
+        [ResponseType(typeof(void))]
+        public IHttpActionResult PutProduct(int id, Product product)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            if (id != product.PoductId)
+            {
+                return BadRequest();
+            }
+
+            db.Entry(product).State = EntityState.Modified;
+
+            try
+            {
+                db.SaveChanges();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!ProductExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return StatusCode(HttpStatusCode.NoContent);
+        }
+
+        // POST: api/Products
+        [Authorize]
+        [ResponseType(typeof(Product))]
+        public IHttpActionResult PostProduct(Product product)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            db.Products.Add(product);
+            db.SaveChanges();
+
+            return CreatedAtRoute("DefaultApi", new { id = product.PoductId }, product);
+        }
+
+        // DELETE: api/Products/5
+        [Authorize]
+        [ResponseType(typeof(Product))]
+        public IHttpActionResult DeleteProduct(int id)
+        {
+            Product product = db.Products.Find(id);
+            if (product == null)
+            {
+                return NotFound();
+            }
+
+            db.Products.Remove(product);
+            db.SaveChanges();
+
+            return Ok(product);
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        private bool ProductExists(int id)
+        {
+            return db.Products.Count(e => e.PoductId == id) > 0;
+        }
+    }
+}
